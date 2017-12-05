abb2ff90a1e2cbd550e6bfed89b8e6b92ed53f97
Simin Hong <simin@Simins-MacBook-Pro.local>
2017-12-05 14:03:31 +0800

    file added

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -1,10 +1,25 @@
+#-------------------------------------------------------------------------------
+# Author: Kostas Oreopoulos <kostas.oreopoulos@gmail.com>
+# Date:   28.08.17
+#-------------------------------------------------------------------------------
 
+"""
+A simple PID controller
+"""
+
+# pylint: disable=invalid-name
+# pylint: disable=too-many-instance-attributes
 MIN_NUM = float('-inf')
 MAX_NUM = float('inf')
 
 
 class PID(object):
-    def __init__(self, kp, ki, kd, mn=MIN_NUM, mx=MAX_NUM):
+    """
+    A simple PID controller object
+    """
+
+    def __init__(self, kp=0.0, ki=0.0, kd=0.0, mn=MIN_NUM, mx=MAX_NUM, lowpass_filter=None):
+        # pylint: disable=too-many-arguments
         self.kp = kp
         self.ki = ki
         self.kd = kd
@@ -12,18 +27,41 @@ class PID(object):
         self.max = mx
 
         self.int_val = self.last_int_val = self.last_error = 0.
+        self.filter = lowpass_filter
 
     def reset(self):
+        """
+        Resets Accumulated Error
+        """
+
         self.int_val = 0.0
         self.last_int_val = 0.0
 
+        if self.filter is not None:
+            self.filter.last_val = 0.0
+
     def step(self, error, sample_time):
+        """
+        Given the CTE (cross track error) and the time elapsed
+        since previous step it return a new value
+
+        Args:
+             error (float) : Cross Track Error
+             sample_time (float): The time since the previous calculation
+
+        Returns:
+             val (float) : The value that should be applied to the controller
+        """
+
         self.last_int_val = self.int_val
 
-        integral = self.int_val + error * sample_time;
-        derivative = (error - self.last_error) / sample_time;
+        integral = self.int_val + error * sample_time
+        derivative = (error - self.last_error) / sample_time
 
-        y = self.kp * error + self.ki * self.int_val + self.kd * derivative;
+        if self.filter is not None:
+            derivative = self.filter.filt(derivative)
+
+        y = self.kp * error + self.ki * self.int_val + self.kd * derivative
         val = max(self.min, min(y, self.max))
 
         if val > self.max:
